5d6f7c440f6e6d49d9ab97d4753d7de4a6f7fa46
Hujianboo <466636709@qq.com>
2021-10-09 00:46:52 +0800

    init
    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: hujianbo
+ * @LastEditors: Hujianbo
+-->
+<!--App.vue-->
+<template>
+  <div id="app">
+    <Navigator/>
+  </div>
+</template>
+
+<script>
+import {ref, reactive, defineComponent, onMounted} from '@vue/composition-api'
+import Navigator from './components/Navigator.vue'
+export default defineComponent({
+  name: "App",
+  components:{Navigator},
+  setup(){
+    
+  }
+});
+</script>
+<style lang="scss" scoped>
+#app{
+  height: 100%;
+}
+</style>
---
+++ src/components/Navigator.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: Hujianbo
+ * @Date: 2021-10-09 00:02:30
+ * @LastEditors: Hujianbo
+ * @LastEditTime: 2021-10-09 00:26:02
+ * @FilePath: /talking-frontend/src/components/Navigator.vue
+-->
+<template>
+  <div class="navigator">
+    <Login/>
+  </div>
+</template>
+<script>
+import Login from './Login.vue'
+import {defineComponent} from '@vue/composition-api'
+export default defineComponent ({
+  name: 'Navigator',
+  components: {Login},
+  setup(){
+    console.log('navigator');
+  }
+})
+</script>
+<style scoped lang="scss">
+.navigator{
+  width: 100%;
+  height: 100px;
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding:  20px;
+  // background: blue;
+}
+</style>
